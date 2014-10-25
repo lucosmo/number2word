ee3ac892e086c14fc89ac7387461d5f12343ea45
lucosmo <lukasz.kosmaczewski@gmail.com>
2014-10-25 15:01:04 +0100

    working first version of script converting numbers into words.

---
+++ num2word.php
@@ -0,0 +1,96 @@
+<?php
+/*******************************************************
+***		Author: Lukasz Kosmaczewski					 ***
+***		twitter: lucosmo							 ***
+***		email: lukasz.kosmaczewski@gmail.com		 ***
+********************************************************
+*******************DESCRIPTION**************************
+***  Script converts numbers into words e.g.:     	 ***
+***	 123 -> "one hundred twenty-three"			 	 ***
+***	 Script uses long-scale naming system		 	 ***
+***	 Max value to convert is 999 999 999 999 999 999 ***
+********************************************************/
+
+
+class Num2Word{
+	private $number;
+	private $length;
+	private $digits=array("0"=>"zero","1"=>"one","2"=>"two","3"=>"three","4"=>"four","5"=>"five","6"=>"six","7"=>"seven","8"=>"eight","9"=>"nine");
+	private $digits1=array("0"=>"ten","1"=>"eleven","2"=>"twelve","3"=>"thirteen","4"=>"fourteen","5"=>"fifteen","6"=>"sixteen","7"=>"seventeen","8"=>"eighteen","9"=>"nineteen");
+	private $digits2=array("1"=>"ten","2"=>"twenty","3"=>"thirty","4"=>"forty","5"=>"fifty","6"=>"sixty","7"=>"seventy","8"=>"eighty","9"=>"ninety");
+	private $digits3=array(1=>"hundred",2=>"thousand",3=>"million", 4=>"milliard",5=>"billion", 6=>"billiard"); 
+	private function checkNum(&$num)
+	{
+		$search=array(" ","\t","\n","\r","\0","\x0B"); 
+		$num=str_replace($search,"",$num); //search for white characters and remove them
+		if(strlen($num)>1 && $num[0]=="0") return false; //if number start from '0' return false
+		return ctype_digit($num);
+	}
+	
+	/* Converting 2-digit numbers */
+	private function readDouble($dblDig)
+	{
+		if($dblDig[0]==1) // check if it's value bewteen 10 and 19 
+			return (string)$this->digits1[(string)$dblDig[1]];
+		elseif($dblDig[1]==0) // check if it's round double digit number (10,20,30...)
+			return (string)$this->digits2[(string)$dblDig[0]];
+		else return (string)$this->digits2[(string)$dblDig[0]]."-".$this->digits[(string)$dblDig[1]]; //everything else about two digit numbers
+		
+	}
+	
+	/* converting 3-digit numbers (default)*/
+	private function readTriple($triple)
+	{
+		$word="";
+		$subhpieces=strlen($triple); 
+		if($subhpieces==3)
+		{
+			$word=$this->digits[(string)$triple[0]]." hundred ".$this->readDouble(substr($triple,1,2));
+		}
+		elseif($subhpieces==2) $word=$this->readDouble($triple);
+		else {
+		$word=$this->digits[(string)$triple];
+		}
+		return $word;
+	}
+	
+	/* whole number is divided here into max 3-digits parts*/
+	/* first part consists of 1-3 digits */ 
+	private function readNum($num)
+	{
+		$word="";
+		if($this->length>3)
+		{
+			$extraDigits=$this->length%3;
+			if(!$extraDigits) 
+				$pieces=str_split($num,3);
+			else
+				$pieces=array_merge(array(substr($num,0,$extraDigits)), str_split(substr($num,$extraDigits),3));
+			$hpieces=count($pieces);
+			if($hpieces>1)
+			for($i=$hpieces;$i>0;$i--)  //variable $i lets choose name of number from digits3 array
+			{
+				if($i==1) $word.=$this->readTriple($pieces[$hpieces-$i]);
+				else
+				$word.=$this->readTriple($pieces[$hpieces-$i])." ".$this->digits3[$i]." "; 
+			}
+			else $word=$this->readTriple($pieces[0]); //it's just one part to work on it - 3-digit number less or equal 999
+		}
+		else $word=$this->readTriple($num);
+		echo $word;
+	}
+	
+	function __construct($num)
+	{
+		if($this->checkNum($num))
+		{
+			$this->number=$num;
+			$this->length=strlen($num);
+		}
+		else die("Error: It isn't a number");
+	}
+	function go()
+	{
+		return $this->readNum($this->number);
+	}
+}
