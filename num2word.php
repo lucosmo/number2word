f4616ce73b40d3b10e092eb394f8bc770812067a
lucosmo <lukasz.kosmaczewski@gmail.com>
2021-12-11 21:04:34 +0000

    Symfony coding style

--- num2word.php
+++ num2word.php
@@ -1,14 +1,14 @@
 <?php
 /*
-*		Author: Lukasz Kosmaczewski
-*		twitter: lucosmo
-*		email: lukasz.kosmaczewski@gmail.com
+*	Author: Lukasz Kosmaczewski
+*	twitter: lucosmo
+*	email: lukasz.kosmaczewski@gmail.com
 *
 *
-*  Script converts numbers into words e.g.:
-*	 123 -> "one hundred twenty-three"
-*	 Script uses long-scale naming system
-*	 Max value to convert is 999 999 999 999 999 999
+* Script converts numbers (digits) into word representation e.g.:
+*	123 -> "one hundred twenty-three"
+*	Script uses long-scale naming system
+*	Max value to convert is 999 999 999 999 999 999
 */
 
 
@@ -23,10 +23,10 @@ class Num2Word{
 	 */
 	private $length;
 
-	private $digits=array("0"=>"zero","1"=>"one","2"=>"two","3"=>"three","4"=>"four","5"=>"five","6"=>"six","7"=>"seven","8"=>"eight","9"=>"nine");
-	private $digits1=array("0"=>"ten","1"=>"eleven","2"=>"twelve","3"=>"thirteen","4"=>"fourteen","5"=>"fifteen","6"=>"sixteen","7"=>"seventeen","8"=>"eighteen","9"=>"nineteen");
-	private $digits2=array("1"=>"ten","2"=>"twenty","3"=>"thirty","4"=>"forty","5"=>"fifty","6"=>"sixty","7"=>"seventy","8"=>"eighty","9"=>"ninety");
-	private $digits3=array(1=>"hundred",2=>"thousand",3=>"million", 4=>"milliard",5=>"billion", 6=>"billiard");
+	private $digits = array("0"=>"zero","1"=>"one","2"=>"two","3"=>"three","4"=>"four","5"=>"five","6"=>"six","7"=>"seven","8"=>"eight","9"=>"nine");
+	private $digits1 = array("0"=>"ten","1"=>"eleven","2"=>"twelve","3"=>"thirteen","4"=>"fourteen","5"=>"fifteen","6"=>"sixteen","7"=>"seventeen","8"=>"eighteen","9"=>"nineteen");
+	private $digits2 = array("1"=>"ten","2"=>"twenty","3"=>"thirty","4"=>"forty","5"=>"fifty","6"=>"sixty","7"=>"seventy","8"=>"eighty","9"=>"ninety");
+	private $digits3 = array(1=>"hundred",2=>"thousand",3=>"million", 4=>"milliard",5=>"billion", 6=>"billiard");
 
 	/**
 	 * @param string $num reference to string representing number
@@ -40,7 +40,11 @@ class Num2Word{
 		return ctype_digit($num);
 	}
 
-	/* Converting 2-digit numbers */
+	/**
+	 * Converting 2-digit numbers
+	 * @param string $num reference to string representing number
+	 * @return boolean
+	*/
 	private function readDouble($dblDig)
 	{
 		if($dblDig[0]==1) // check if it's value bewteen 10 and 19
